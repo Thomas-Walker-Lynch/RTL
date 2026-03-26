52066bcba444a31cc27f87bb477216eeae8e8ca2
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2026-03-26 03:41:30 +0000

    tree2 control, config work

--- developer/authored/elisp/RT-tree_test.el
+++ developer/authored/elisp/RT-tree_test.el
@@ -1,5 +1,5 @@
 
-(let 
+  (let 
     (
       (test_ast '(1 (2 3 4 (5 6 7)) 8))
       )
@@ -13,4 +13,36 @@
       (insert "\n=== Decoded AST ===\n")
       (insert (format "%S\n" (RT-tree·decode binary_tape 5)))
       ))
+
+=== Debug Print ===
+    | 1
+ 10 | 2
+  0 | 3
+  0 | 4
+111 | 5
+  0 | 6
+110 | 7
+110 | 8
+
+=== Decoded AST ===
+(1 (2 3 4 (5 6 7)) 8)
+
+
+
+  (let 
+    (
+      (test_ast '(a (b)))
+      )
+    (let 
+      (
+        (binary_tape (RT-tree·encode test_ast 5))
+        )
+      (insert "\n")
+      (insert "\n=== Debug Print ===\n")
+      (insert (RT-tree·debug-print binary_tape 5))
+      (insert "\n=== Decoded AST ===\n")
+      (insert (format "%S\n" (RT-tree·decode binary_tape 5)))
+      ))
+
+
 
